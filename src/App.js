82a3b4b4165e7719cb6db6be140b4b2a1eef30df
SMIT\smit <smitkakadiya2003@gmail.com>
2023-08-02 10:59:36 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
 
-    const [no , setNo] = useState (0)
-    const [result , setResult] = useState (0)
+  const [count,setCount] = useState(0);
+  const cntPlus = () =>{
+    setCount(
+      count + 1
+    )
+  }
 
-    const btnPlus = () => {
-      setNo( no + 1 )
+  const cntMin = () => {
+    if (count > 0) {
+      setCount(
+        count - 1
+        )
+      }
+      alert("You Are Not Decrement");
     }
-    useEffect (() => {
-      console.log(no);
-    },[no])
+  
+    const cntReset = () =>{
+      setCount(
+        0
+      )
+    }
   return (
     <>
-    <h1>No :- {no}</h1>
-    <button onClick={()=> btnPlus()}>Click</button>
+      <div className="container my-5">
+        <div className="card text-center my-5">
+          <div className="card-body">
+            <h1>You can Increment, Decrement and Reset.</h1>
+            <div className="my-5">
+              <h2 className="my-5">{count}</h2>
+              <button className="btn btn-success mx-3" onClick={() => cntPlus()}>Increment</button>
+              <button className="btn btn-secondary mx-3" onClick={() => cntReset()}>Reset</button>
+              <button className="btn btn-danger mx-3" onClick={() => cntMin()}>Decrement</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
